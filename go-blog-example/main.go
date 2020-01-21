72202ce8e63cfbe6c2d632b7fc53d636e480ac3d
Akanibekuly <student@ALEM-316ed2.alem.school>
2020-01-22 05:26:57 +0600

    first commit

---
+++ go-blog-example/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	fmt.Println("Listening on port :3000")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello world")
+	})
+	http.ListenAndServe(":3000", nil)
+}
